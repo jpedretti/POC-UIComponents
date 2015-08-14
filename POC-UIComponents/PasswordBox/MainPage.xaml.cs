ce065199df8751fb3fb72a05bdc144c660f33610
jpedretti <jpedretti@gmail.com>
2015-08-14 11:16:11 -0300

    Teste na tela

--- POC-UIComponents/PasswordBox/MainPage.xaml.cs
+++ POC-UIComponents/PasswordBox/MainPage.xaml.cs
@@ -44,5 +44,10 @@ namespace PasswordBox
             // If you are using the NavigationHelper provided by some templates,
             // this event is handled for you.
         }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+            textBlocMostraSenha.Text = custonPasswordBox.TypedPassword;
+        }
     }
 }
