a064043023c62e7ee5832f1f930688effc6601ca
Jeferson Santos <jefersons@ciandt.com>
2015-08-21 14:42:22 -0300

    Adicionando Address Book com Stick Letters e Groups.

---
+++ POC-UIComponents/POC-UIComponents-App/Model/AddressBookModel.cs
@@ -0,0 +1,127 @@
+﻿using POC_UIComponents_App.Infrastructure;
+using System;
+using System.Collections.Generic;
+using System.Globalization;
+using System.Linq;
+using System.Text;
+using System.Text.RegularExpressions;
+using System.Threading.Tasks;
+using Windows.Globalization.Collation;
+
+namespace POC_UIComponents_App.Model
+{
+    public class AlphaKeyGroup<T> : List<T>
+    {
+        /// <summary>
+        /// The delegate that is used to get the key information.
+        /// </summary>
+        /// <param name="item">An object of type T</param>
+        /// <returns>The key value to use for this object</returns>
+        public delegate string GetKeyDelegate(T item);
+
+        /// <summary>
+        /// The Key of this group.
+        /// </summary>
+        public string Key { get; private set; }
+
+        /// <summary>
+        /// Public constructor.
+        /// </summary>
+        /// <param name="key">The key for this group.</param>
+        public AlphaKeyGroup(string key)
+        {
+            Key = key;
+        }
+
+        /// <summary>
+        /// Create a list of AlphaGroup<T> with keys set by a SortedLocaleGrouping.
+        /// </summary>
+        /// <param name="slg">The </param>
+        /// <returns>Theitems source for a LongListSelector</returns>
+        private static List<AlphaKeyGroup<T>> CreateGroups(CharacterGroupings slg)
+        {
+            List<AlphaKeyGroup<T>> list = new List<AlphaKeyGroup<T>>();
+
+            foreach (CharacterGrouping key in slg)
+            {
+                if (string.IsNullOrWhiteSpace(key.Label) == false)
+                    list.Add(new AlphaKeyGroup<T>(key.Label));
+            }
+
+            return list;
+        }
+
+        /// <summary>
+        /// Create a list of AlphaGroup<T> with keys set by a SortedLocaleGrouping.
+        /// </summary>
+        /// <param name="items">The items to place in the groups.</param>
+        /// <param name="ci">The CultureInfo to group and sort by.</param>
+        /// <param name="getKey">A delegate to get the key from an item.</param>
+        /// <param name="sort">Will sort the data if true.</param>
+        /// <returns>An items source for a LongListSelector</returns>
+        public static List<AlphaKeyGroup<T>> CreateGroups(IEnumerable<T> items, CultureInfo ci, GetKeyDelegate getKey, bool sort)
+        {
+            CharacterGroupings slg = new CharacterGroupings();
+            List<AlphaKeyGroup<T>> list = CreateGroups(slg);
+
+            foreach (T item in items)
+            {
+                string index = "";
+                index = slg.Lookup(getKey(item));
+                if (string.IsNullOrEmpty(index) == false)
+                {
+                    list.Find(a => a.Key == index).Add(item);
+                }
+            }
+
+            if (sort)
+            {
+                foreach (AlphaKeyGroup<T> group in list)
+                {
+                    group.Sort((c0, c1) => { return ci.CompareInfo.Compare(getKey(c0), getKey(c1)); });
+                }
+            }
+
+            return list;
+        }
+
+    }
+
+    public class AddressBookEntryModel
+    {
+        public string FullName { get; set; }
+
+        public string BankName { get; set; }
+
+        public string Account { get; set; }
+
+        public string Phone { get; set; }
+
+        public string PhotoPath { get; set; }
+
+        public string Initials { get; set; }
+
+        public Windows.UI.Xaml.Visibility Flag { get; set; }
+
+        public AddressBookEntryModel(string fullName, string bankName, string account, string phone, string photoPath, bool flag = false)
+        {
+            this.FullName = fullName;
+            this.BankName = bankName;
+            this.Account = account;
+            this.Phone = phone;
+            this.PhotoPath = photoPath;
+            this.Initials = ExtractNameInitials(fullName);
+            this.Flag = flag ? Windows.UI.Xaml.Visibility.Visible : Windows.UI.Xaml.Visibility.Collapsed;
+        }
+
+        private string ExtractNameInitials(string fullname)
+        {
+            Regex initials = new Regex(@"(\b[a-zA-Z])[a-zA-Z]* ?");
+
+            string result = initials.Replace(fullname, "$1");
+
+            return result;
+        }
+    }
+
+}
---
+++ POC-UIComponents/POC-UIComponents-App/ViewModels/AddressBookPageViewModel.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Microsoft.Practices.Prism.Commands;
+using Microsoft.Practices.Prism.Mvvm;
+using Microsoft.Practices.Prism.Mvvm.Interfaces;
+using POC_UIComponents_App.Model;
+using POC_UIComponents_App.Infrastructure;
+using System.Globalization;
+using Windows.UI.Xaml.Data;
+using System.Collections.ObjectModel;
+
+namespace POC_UIComponents_App.ViewModels
+{
+    public class AddressBookPageViewModel : ViewModel
+    {
+        private INavigationService _navigationService;
+
+
+        private List<AlphaKeyGroup<AddressBookEntryModel>> _addressGroups = new List<AlphaKeyGroup<AddressBookEntryModel>>();
+
+        public List<AlphaKeyGroup<AddressBookEntryModel>> AddressGroups
+        {
+            get
+            {
+                return _addressGroups;
+            }
+            set
+            {
+                SetProperty(ref _addressGroups, value);
+            }
+        }
+
+
+
+        public AddressBookPageViewModel(INavigationService navigationService)
+        {
+            _navigationService = navigationService;
+        }
+
+
+        public override void OnNavigatedTo(object navigationParameter, Windows.UI.Xaml.Navigation.NavigationMode navigationMode, Dictionary<string, object> viewModelState)
+        {
+            GenerateDummyData();
+
+            base.OnNavigatedTo(navigationParameter, navigationMode, viewModelState);
+        }
+
+        private void GenerateDummyData()
+        {
+            List<AddressBookEntryModel> source = new List<AddressBookEntryModel>();
+            source.Add(new AddressBookEntryModel("Ringo Pumpkin", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", ""));
+            source.Add(new AddressBookEntryModel("Vincent Vega", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "", true));
+            source.Add(new AddressBookEntryModel("Yolanda Honey Bunny", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", ""));
+            source.Add(new AddressBookEntryModel("Jules Winnfield", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", ""));
+            source.Add(new AddressBookEntryModel("Marvin", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "", true));
+            source.Add(new AddressBookEntryModel("Brett", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", ""));
+            source.Add(new AddressBookEntryModel("Roger", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "", true));
+            source.Add(new AddressBookEntryModel("Butch Coolidge", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", ""));
+            source.Add(new AddressBookEntryModel("Marsellus Wallace", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg", true));
+            source.Add(new AddressBookEntryModel("Paul English Bob", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg"));
+            source.Add(new AddressBookEntryModel("Trudi", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg", true));
+            source.Add(new AddressBookEntryModel("Jody", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg", true));
+            source.Add(new AddressBookEntryModel("Lance", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg", true));
+            source.Add(new AddressBookEntryModel("Mia Wallace", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg"));
+            source.Add(new AddressBookEntryModel("Bob Bogle", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg"));
+            source.Add(new AddressBookEntryModel("Quentin", "Citi", "Ag. 1234 | CC. 1231231", "99999-0011", null, true));
+            source.Add(new AddressBookEntryModel("Jeferson", "Zed", "Ag. 1234 | CC. 1231231", "99999-0011", "/Assets/contact_photo.jpg", true));
+
+            AddressGroups = AlphaKeyGroup<AddressBookEntryModel>.CreateGroups(source, CultureInfo.CurrentUICulture, s => s.FullName, true);
+        }
+
+
+
+
+
+    }
+}
